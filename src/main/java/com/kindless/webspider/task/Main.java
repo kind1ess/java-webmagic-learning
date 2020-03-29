1eba076d1d2a715efb320d7ffbd8e3191b584422
kindless <2411347677@qq.com>
2020-03-29 17:45:21 +0800

    第六次提交

--- src/main/java/com/kindless/webspider/task/Main.java
+++ src/main/java/com/kindless/webspider/task/Main.java
@@ -7,7 +7,7 @@ import us.codecraft.webmagic.scheduler.QueueScheduler;
 public class Main {
     public static void main(String[] args) {
         Spider.create(new BilibiliProcessor())
-                .addUrl("https://search.bilibili.com/all?keyword=%E8%8B%B1%E9%9B%84%E8%81%94%E7%9B%9F&from_source=nav_search_new")
+                .addUrl("https://search.bilibili.com/all?keyword=%E8%81%94%E6%92%AD")
                 .setScheduler(new QueueScheduler().setDuplicateRemover(new BloomFilterDuplicateRemover(100000)))
                 .thread(100)
                 .addPipeline(new BilibiliPipeline())
